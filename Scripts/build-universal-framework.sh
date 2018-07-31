70f384afd7cfd62bd45702705c7f85a31d3a8dfc
Richard Stelling <rich@lionheartapplications.com>
2018-07-31 21:31:32 +0100

    Xcode 10 Fixes
    Xcode 10 beta - `${CONFIGURATION}` seems to be missing, so I've added ${CONFIGURATION:-Release}

--- Scripts/build-universal-framework.sh
+++ Scripts/build-universal-framework.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/sh -x
+
+# NB: Xcode 10 - ${CONFIGURATION} seems to be missing, so I've added ${CONFIGURATION:-Release}
 
 UNIVERSAL_OUTPUT_FOLDER="$2"
 
@@ -7,16 +9,16 @@ mkdir -p "${UNIVERSAL_OUTPUT_FOLDER}"
 rm -rf "${UNIVERSAL_OUTPUT_FOLDER}/$1.framework"
 
 # Step 1. Build Device and Simulator versions
-xcodebuild -target "$1" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos  BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
-xcodebuild -target "$1" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphonesimulator BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+xcodebuild -target "$1" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION:-Release} -sdk iphoneos  BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+xcodebuild -target "$1" ONLY_ACTIVE_ARCH=NO -configuration $${CONFIGURATION:-Release} -sdk iphonesimulator BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
 
 # Step 2. Copy the framework structure to the universal folder
-cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/$1.framework" "${UNIVERSAL_OUTPUT_FOLDER}/"
+cp -R "${BUILD_DIR}/${CONFIGURATION:-Release}-iphoneos/$1.framework" "${UNIVERSAL_OUTPUT_FOLDER}/"
 
 # Step 3. Create universal binary file using lipo and place the combined executable in the copied framework directory
-lipo -create -output "${UNIVERSAL_OUTPUT_FOLDER}/$1.framework/$1" "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/$1.framework/$1" "${BUILD_DIR}/${CONFIGURATION}-iphoneos/$1.framework/$1"
+lipo -create -output "${UNIVERSAL_OUTPUT_FOLDER}/$1.framework/$1" "${BUILD_DIR}/${CONFIGURATION:-Release}-iphonesimulator/$1.framework/$1" "${BUILD_DIR}/${CONFIGURATION:-Release}}-iphoneos/$1.framework/$1"
 
 # Step 3b. Copy the simultor swiftdoc and swiftmodules in to the framework structure
 echo "${UNIVERSAL_OUTPUT_FOLDER}/$1.framework/Modules/$1.swiftmodule/"
-cp "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/$1.framework/Modules/$1.swiftmodule/i386."* "${UNIVERSAL_OUTPUT_FOLDER}/$1.framework/Modules/$1.swiftmodule/" 2>/dev/null || :
-cp "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/$1.framework/Modules/$1.swiftmodule/x86_64."* "${UNIVERSAL_OUTPUT_FOLDER}/$1.framework/Modules/$1.swiftmodule/" 2>/dev/null || :
+cp "${BUILD_DIR}/${CONFIGURATION:-Release}-iphonesimulator/$1.framework/Modules/$1.swiftmodule/i386."* "${UNIVERSAL_OUTPUT_FOLDER}/$1.framework/Modules/$1.swiftmodule/" 2>/dev/null || :
+cp "${BUILD_DIR}/${CONFIGURATION:-Release}-iphonesimulator/$1.framework/Modules/$1.swiftmodule/x86_64."* "${UNIVERSAL_OUTPUT_FOLDER}/$1.framework/Modules/$1.swiftmodule/" 2>/dev/null || :
